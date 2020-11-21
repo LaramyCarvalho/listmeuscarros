2c162e3cea8f3b97b5da7f9956effc0514279583
laramycarvalho <laramycarvalho@gmail.com>
2020-11-20 23:33:22 -0300

    20/11/2020

--- lib/main.dart
+++ lib/main.dart
@@ -4,63 +4,60 @@ import 'package:flutter/material.dart';
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ListMeusCarros("1"));
 }
 
-class App extends StatelessWidget {
-  // Create the initilization Future outside of `build`:
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return Text("ops");
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return ListMeusCarros("1");
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return Text("Loading");
-      },
-    );
-  }
-}
-
-class ListMeusCarros extends StatelessWidget {
+class ListMeusCarros extends StatefulWidget {
   final String documentId;
+  ListMeusCarros(
+    this.documentId, {
+    Key key,
+  }) : super(key: key);
 
-  ListMeusCarros(this.documentId);
+  @override
+  _ListMeusCarrosState createState() => _ListMeusCarrosState();
+}
+
+class _ListMeusCarrosState extends State<ListMeusCarros> {
+  CollectionReference meusCarros =
+      FirebaseFirestore.instance.collection('meus_carros');
+
+  List<Map<String, dynamic>> data = <Map<String, dynamic>>[];
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection('meus_carros')
+        .get()
+        .then((value) => setState(() {
+              value.docs.map((e) => print(e.data())).toList();
+            }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference meus_carros =
-        FirebaseFirestore.instance.collection('meus_carros');
-
-    return FutureBuilder<DocumentSnapshot>(
-      future: meus_carros.doc(documentId).get(),
-      builder:
-          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data = snapshot.data.data();
-          return Text(
-              "Full Name: ${data['anoFabricacao']} ${data['anoModelo']} ${data['modelo']} ${data['marca']} ${data['placa']} ");
-        }
-
-        return Text("loading");
-      },
+    return MaterialApp(
+      title: 'CARROS',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MEUS CARROS'),
+        ),
+        body: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            var carro = data[index];
+            return Padding(
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                  "Meu carro$index: ${carro['anoFabricacao']} ${carro['anoModelo']} ${carro['modelo']} ${carro['marca']} ${carro['placa']} "),
+            );
+          },
+        ),
+      ),
     );
   }
 }
